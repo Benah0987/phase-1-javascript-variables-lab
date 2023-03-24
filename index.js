485a616d43040dda24de98a540ac42e6b8b95085
Benah0987 <88454033+Benah0987@users.noreply.github.com>
2023-03-24 14:29:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+
+
+// Define the company name as a constant
+const companyName = 'Scuber';
+
+// Define the most profitable neighborhood using let
+let mostProfitableNeighborhood = 'Chelsea';
+
+// Define the company CEO using let
+let companyCeo = 'Susan Smith';
+
+s
+
